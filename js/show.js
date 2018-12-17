12c206bfeaccfa972222ed5a097aa5598e9214ea
marta <marta.cattaneo.96@gmail.com>
2018-12-17 13:34:02 +0100

    rifotografia

--- js/show.js
+++ js/show.js
@@ -1,6 +1,21 @@
-jQuery(function(){
-    jQuery('.showSingle').click(function(){
-          jQuery('.targetDiv').hide();
-          jQuery('#div'+$(this).attr('target')).show();
+jQuery(function () {
+    jQuery('.showSingle').click(function () {
+        jQuery('.targetDiv').hide();
+        jQuery('#div' + $(this).attr('target')).show();
     });
 });
+
+
+
+function constrain(v, min, max) {
+    return Math.max(Math.min(v, max), min);
+}
+
+document.getElementById('imageA').onmousemove = function (e) {
+    var maxWidth = this.width
+    var left = this.getBoundingClientRect().left;
+    var x = e.clientX - left;
+    var opacity = constrain(x / maxWidth, 0, 0.9999);
+    //console.log(left, maxWidth, x, opacity);
+    document.getElementById('imageB').style.opacity = opacity;
+}
